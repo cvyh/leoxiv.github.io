cd468284d3b4eae9e62ac2479ce9e6f4b1df41b8
leoxiv <iamyan@live.cn>
2018-11-23 13:47:07 +0800

    locales fixed

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,12 @@
 module.exports = {
-    title: '一个奇奇怪怪的地方',
-    description: '前往另一些奇怪的地方',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: '一个奇奇怪怪的地方',
+      description: '前往另一些奇怪的地方',
+    },
 
+  },
     themeConfig: {
       search: false,
       navbar: false
